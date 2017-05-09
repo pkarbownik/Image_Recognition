cf990de576da21df1bc23b66d0c6d397efae1643
Marek <marek255trojan@gmail.com>
2017-05-09 02:42:34 +0200

    HOG related functions implemented

--- Histogram_of_Oriented_Gradients/gradient.cpp
+++ Histogram_of_Oriented_Gradients/gradient.cpp
@@ -1,106 +1,121 @@
 #include "gradient.h"
 #include "ui_gradient.h"
 
-
 Gradient::Gradient(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Gradient)
 {
     ui->setupUi(this);
 
-    //source: http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/sobel_derivatives/sobel_derivatives.html
-    Mat src, src_gray;
-    Mat grad;
-    int scale = 1;
-    int delta = 0;
-    int ddepth = CV_16S;
-
-    int c;
-
-    /// Load an image
-    src = imread( "..\\Histogram_of_Oriented_Gradients\\image\\400_317.jpg" );
-    if( !src.data )
-    {
-         cout<<"Can't open image";
-    }
-    //Display original
-    namedWindow( "Source", CV_WINDOW_AUTOSIZE );
-    imshow( "Source", src );
-
-    GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
-
-    namedWindow( "After GaussianBlur", CV_WINDOW_AUTOSIZE );
-    imshow( "After GaussianBlur", src );
-    /// Convert it to gray
-    cvtColor( src, src_gray, CV_BGR2GRAY );
-
-    namedWindow( "After Convert to Gray", CV_WINDOW_AUTOSIZE );
-    imshow( "After Convert to Gray", src_gray );
-    /// Create window
-
-
-    /// Generate grad_x and grad_y
-    Mat grad_x_sob, grad_x_sch, grad_y;
-    Mat abs_grad_x_sob, abs_grad_x_sch, abs_grad_y;
-
-    /// Gradient X
-    Scharr( src_gray, grad_x_sch, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
-    convertScaleAbs( grad_x_sch, abs_grad_x_sch );
-    Sobel( src_gray, grad_x_sob, ddepth, 1, 0, 3, scale, delta, BORDER_DEFAULT );
-    convertScaleAbs( grad_x_sob, abs_grad_x_sob );
-
-
-    namedWindow( "After X Sobel Gradient Convert", CV_WINDOW_AUTOSIZE );
-    imshow( "After X Sobel Gradient Convert", abs_grad_x_sob );
-    namedWindow( "After X Scharr Gradient Convert", CV_WINDOW_AUTOSIZE );
-    imshow( "After X Scharr Gradient Convert", abs_grad_x_sch );
-    /// Gradient Y
-    //Scharr( src_gray, grad_y, ddepth, 0, 1, scale, delta, BORDER_DEFAULT );
-    Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
-    convertScaleAbs( grad_y, abs_grad_y );
-    namedWindow( "After Y Gradient Convert", CV_WINDOW_AUTOSIZE );
-    imshow( "After Y Gradient Convert", abs_grad_y );
-    /// Total Gradient (approximate)
-    addWeighted( abs_grad_x_sob, 0.5, abs_grad_y, 0.5, 0, grad );
-    // C++ Calculate gradient magnitude and direction (in degrees)
-    namedWindow( "Sobel Demo - Simple Edge Detector", CV_WINDOW_AUTOSIZE );
-    imshow( "Sobel Demo - Simple Edge Detector", grad );
-
-
-
-    // C++ gradient calculation.
-    // Read image
-  /*  Mat img = imread("..\\Histogram_of_Oriented_Gradients\\image\\400_317.jpg");
-    img.convertTo(img, CV_32F, 1/255.0); //CV_32F is float - the pixel can have any value between 0-1.0
+    Mat img = imread(imageDirPath + filename, CV_8UC1);
+    int x_blocks = img.cols / BLOCK_SIZE;
+    int y_blocks = img.rows / BLOCK_SIZE;
+    printf("File: %s, width: %d, height: %d\n", filename.c_str(), img.cols, img.rows);
+    printf("x_blocks: %d, y_blocks: %d\n", x_blocks, y_blocks);
 
     // Calculate gradients gx, gy
     Mat gx, gy;
     Sobel(img, gx, CV_32F, 1, 0, 1);
     Sobel(img, gy, CV_32F, 0, 1, 1);
-    imshow( "GX", gx );
-    imshow( "GY", gy );
-    // C++ Calculate gradient magnitude and direction (in degrees)
+
+    // Calculate gradient magnitude and direction (in degrees)
     Mat mag, angle;
     cartToPolar(gx, gy, mag, angle, 1);
-    imshow( "MAG", angle );
+    // Process angles values to provide 0 to 180.0 range instead of 0 to 360.0
+    modulo180degrees(angle);
 
+    // Perform histogram computations for blocks in entire image
+    QVector<QVector<QVector<float>>> histogram(x_blocks, QVector<QVector<float>> (y_blocks, QVector<float>(9, 0.0)));
+    imageToHistogram(histogram, mag, angle, BLOCK_SIZE, x_blocks, y_blocks);
 
+    // Prepare final outputVector for neural network by normalizing block histograms
+    QVector<float> outputVector = HOG_normalization(histogram, x_blocks, y_blocks);
 
+    // Open CSV file and save outputVector
+    QFile csvFile(QString(csvDirPath.c_str()) + "test.csv");
+    if (!csvFile.open(QIODevice::WriteOnly | QIODevice::Text))
+        return;
+    QTextStream csvStream(&csvFile);
+    for(int i = 0; i < outputVector.size(); i++)
+        csvStream << qSetRealNumberPrecision(6) << outputVector[i] << ',';
+    csvFile.close();
 
-    /*Mat img = imread("..\\Histogram_of_Oriented_Gradients\\image\\400_317.jpg");
-    Mat grey;
-    cvtColor(img, grey, CV_BGR2GRAY);
-
-    Mat sobelx;
-    Sobel(grey, sobelx, CV_32F, 1, 0);
-
+    /*
     double minVal, maxVal;
-    minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
-    Mat draw;
-    sobelx.convertTo(draw, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
+    minMaxLoc(mag, &minVal, &maxVal);
+    printf("Mag Max: %f, min: %f\n", (maxVal), (minVal));
+    minMaxLoc(angle, &minVal, &maxVal);
+    printf("Angle Max: %f, min: %f\n", (maxVal), (minVal));
+    */
+    mag.convertTo(mag, CV_8U);
+    angle.convertTo(angle, CV_8U);
+    imshow( "MAG", mag );
+    imshow( "ANGLE", angle );
 
-    namedWindow("image", CV_WINDOW_AUTOSIZE);
-    imshow("image", draw);*/
+}
+
+void Gradient::imagePatchToHistogram(QVector<float> &patchHistogram, const Mat &mag, const Mat &angle, int x_block, int y_block, int blockSize){
+    float lowerAngle, currentAngle, currentMag, leftFraction, rightFraction;
+    int leftIndex;
+    for (int row = 0; row < blockSize; row++){
+        for(int col = 0; col < blockSize; col++){
+            currentAngle = angle.at<float>(Point(x_block * blockSize + col, y_block * blockSize + row));
+            currentMag = mag.at<float>(Point(x_block * blockSize + col, y_block * blockSize + row));
+            leftIndex = int(currentAngle) / 20;
+            lowerAngle = float(leftIndex) * 20.0;
+            leftFraction = (currentAngle - lowerAngle) / 20.0;
+            rightFraction = 1.0 - leftFraction;
+            patchHistogram[leftIndex % 9] += (rightFraction * currentMag);
+            patchHistogram[(leftIndex + 1) % 9] += (leftFraction * currentMag);
+        }
+    }
+}
+
+void Gradient::imageToHistogram(QVector<QVector<QVector<float>>> &histogram, const Mat &mag, const Mat &angle, int blockSize, int x_blocks, int y_blocks){
+    for (int x_block = 0; x_block < x_blocks; x_block++){
+        for(int y_block = 0; y_block < y_blocks; y_block++){
+            imagePatchToHistogram(histogram[x_block][y_block], mag, angle, x_block, y_block, blockSize);
+            /*
+            printf("Histogram vector[%d][%d]\t", x_block, y_block);
+            for(int i = 0; i < 9; i++)
+                printf("%0.1f\t", histogram[x_block][y_block][i]);
+            printf("\n");
+            */
+        }
+    }
+}
+
+void Gradient::modulo180degrees(Mat &img){
+    // Parallel execution using C++11 lambda.
+    img.forEach<float>([](float &p, const int * position) -> void {
+        if (p >= 180.0) p -= 180.0;
+    });
+}
+
+QVector<float> Gradient::HOG_normalization(QVector<QVector<QVector<float>>> vector,  const int x_blocks, const int y_blocks){
+    QVector<float> normalized(36 * (x_blocks - 1) * (y_blocks - 1));
+    float temp = 0;
+    int index = 0;
+    for (int i=0; i<x_blocks-1; i++){
+        for(int j=0; j<y_blocks-1; j++){
+            temp = 0;
+            for(int k=0; k<9; k++){
+                temp+=qPow(vector[i][j][k],2);
+                temp+=qPow(vector[i+1][j][k],2);
+                temp+=qPow(vector[i+1][j+1][k],2);
+                temp+=qPow(vector[i][j+1][k],2);
+            }
+            temp=qSqrt(temp);
+            for(int k=0; k<9; k++){
+                normalized[index]=vector[i][j][k]/temp;
+                normalized[index++]=vector[i+1][j][k]/temp;
+                normalized[index++]=vector[i][j+1][k]/temp;
+                normalized[index++]=vector[i+1][j+1][k]/temp;
+            }
+        }
+    }
+    return normalized;
+
 }
 
 Gradient::~Gradient()
--- Histogram_of_Oriented_Gradients/gradient.h
+++ Histogram_of_Oriented_Gradients/gradient.h
@@ -7,8 +7,21 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <stdarg.h>
+#include <QtMath>
+#include <QFile>
+#include <QTextStream>
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/ml/ml.hpp>
+
+#define BLOCK_SIZE 8
+
 using namespace cv;
 using namespace std;
+
+const String imageDirPath("..\\HOG\\image\\");
+const String csvDirPath("..\\HOG\\csv\\");
+const String filename("32_32.png");
 
 namespace Ui {
 class Gradient;
@@ -17,6 +30,12 @@ class Gradient;
 class Gradient : public QMainWindow
 {
     Q_OBJECT
+
+public:
+    void modulo180degrees(Mat &angle);
+    void imagePatchToHistogram(QVector<float> &patchHistogram, const Mat &mag, const Mat &angle, int x_block, int y_block, int blockSize);
+    void imageToHistogram(QVector<QVector<QVector<float>>> &histogram, const Mat &mag, const Mat &angle, int blockSize, int x_blocks, int y_blocks);
+    QVector<float> HOG_normalization(QVector<QVector<QVector<float>>> vector, const int x_blocks, const int y_blocks);
 
 public:
     explicit Gradient(QWidget *parent = 0);
