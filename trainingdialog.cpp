ace9a6d636c80384638695b423cf85fbf0693cd7
pkarbownik <przekar4926@gmail.com>
2017-05-25 13:09:58 +0200

    Added selection of images diretory. Prepared function to train Neural Network with typed in GUI parameters and selected directory path.

--- trainingdialog.cpp
+++ trainingdialog.cpp
@@ -12,3 +12,20 @@ TrainingDialog::~TrainingDialog()
 {
     delete ui;
 }
+
+
+void TrainingDialog::performTrainingProcess(QString ImgDirPath,int inputN, int hiddenN, int outputN, int maxEp, int desiredAccu, float learnRate){
+    ui->textBrowser_trainingLogs->append(QString::number(inputN));
+    ui->textBrowser_trainingLogs->append(QString::number(hiddenN));
+    ui->textBrowser_trainingLogs->append(ImgDirPath);
+}
+
+void TrainingDialog::on_pushButton_startTraining_clicked()
+{
+    performTrainingProcess(imgDirPath,ui->lineEdit_inputNeurons->text().toInt(),ui->lineEdit_hiddenNeurons->text().toInt(),ui->lineEdit_outputNeurons->text().toInt(),ui->lineEdit_maxEpochs->text().toInt(),ui->lineEdit_desiredAccuracy->text().toInt(),ui->lineEdit_learningRate->text().toFloat());
+}
+
+void TrainingDialog::on_pushButton_loadTrainingDirectory_clicked()
+{
+    imgDirPath = QFileDialog::getExistingDirectory();
+}
--- trainingdialog.h
+++ trainingdialog.h
@@ -2,6 +2,8 @@
 #define TRAININGDIALOG_H
 
 #include <QWidget>
+#include <QFileDialog>
+#include <QDebug>
 #include "ui_trainingdialog.h"
 
 namespace Ui {
@@ -15,8 +17,14 @@ class TrainingDialog : public QWidget
 public:
     explicit TrainingDialog(QWidget *parent = 0);
     ~TrainingDialog();
+    void performTrainingProcess(QString ImgDirPath, int inputN, int hiddenN, int outputN, int maxEp, int desiredAccu, float learnRate);
+private slots:
+    void on_pushButton_startTraining_clicked();
+
+    void on_pushButton_loadTrainingDirectory_clicked();
 
 private:
+    QString imgDirPath="no image";
     Ui::TrainingDialog *ui;
 };
 
